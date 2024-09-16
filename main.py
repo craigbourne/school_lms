76bf09c792c0563d099d4b0226d56874924f85f0
Craig Bourne <craigbourne15@gmail.com>
2024-09-16 11:03:37 +0100

    added init homepage and basic styling to test css working

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+# Set up Jinja2 templates
+templates = Jinja2Templates(directory="templates")
+
+# Mount a static files directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get("/")
-async def root():
-    return {"message": "Hello from the void"}
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-    
